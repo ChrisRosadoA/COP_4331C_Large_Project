40fdd7a851d2bdfee5d7698f1ae020b36e38d18d
brimatt16219 <68443696+brimatt16219@users.noreply.github.com>
2023-04-19 20:21:05 -0400

    finish base api

--- cards/frontend/src/components/Login.js
+++ cards/frontend/src/components/Login.js
@@ -13,7 +13,34 @@ function Login()
     {
         event.preventDefault();
 
-        alert('doIt() ' + loginName.value + ' ' + loginPassword.value );
+        var obj = {login:loginName.value,password:loginPassword.value};
+        var js = JSON.stringify(obj);
+
+        try
+        {    
+            const response = await fetch('http://localhost:5000/api/login',
+                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            var res = JSON.parse(await response.text());
+
+            if( res.id <= 0 )
+            {
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
+
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
     };
 
 
