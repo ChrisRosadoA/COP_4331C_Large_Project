d6d05b59d00c4017a7d9b10b9482768a5bf5d0ae
brimatt16219 <68443696+brimatt16219@users.noreply.github.com>
2023-04-26 14:55:59 -0400

    registerjs

---
+++ cards/frontend/src/components/Register.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+function Register()
+{
+    var registerEmail;
+
+    var registerName;
+
+    var registerPassword;
+    var registerPasswordRepeat;
+
+    const [message,setMessage] = useState('');
+
+
+    const doRegister = async event => 
+    {
+        event.preventDefault();
+
+        var obj = {register:registerName.value,password:registerPassword.value};
+        var js = JSON.stringify(obj);
+
+        try
+        {    
+            const response = await fetch('http://localhost:5000/api/login',
+                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            var res = JSON.parse(await response.text());
+
+            if( res.id <= 0 )
+            {
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
+
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
+    };
+
+
+    return(
+      <div id="registerDiv">
+        <form onSubmit={doRegister}>
+        <span id="inner-title">Register</span><br />
+        <input type="text" id="registerName" placeholder="Username" ref={(c) => registerName = c} /><br />
+        <input type="password" id="registerPassword" placeholder="Password" ref={(c) => registerPassword = c} /><br />
+
+        <input type="submit" id="registerButton" class="buttons" value = "Do It"
+          onClick={doRegister} />
+        </form>
+        <span id="loginResult">{message}</span>
+     </div>
+    );
+};
+
+export default Register;
